afda8afbfb7891fb6be3618c946f63e815b370b2
Johanna Pinsirikul <jpinsirikul@sevatec.com>
2016-09-07 11:43:13 -0400

    adding the give point tests

---
+++ src/test/java/dhsFlashSelenium/DHSFlashSelenium/GivePointTest.java
@@ -0,0 +1,27 @@
+package dhsFlashSelenium.DHSFlashSelenium;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+import dhsFlashSelenium.Pages.GivePoint;
+
+public class GivePointTest extends AbstractTest {
+	GivePoint givePoint;
+	String employeeName = "Julia Brundage";
+
+	@Test (priority = 0)
+	public void submitPointForm() {
+		givePoint = new GivePoint(driver);
+		waitDriver.until(ExpectedConditions.visibilityOf(givePoint.getPointForm()));
+		givePoint.selectColleague(employeeName);
+		givePoint.clickGivePoint();
+	}
+	
+	@Test (priority = 1)
+	public void submitEmptyPointForm() {
+		givePoint = new GivePoint(driver);
+		waitDriver.until(ExpectedConditions.visibilityOf(givePoint.getPointForm()));
+		givePoint.clickGivePoint();
+	}
+
+}
--- src/test/java/dhsFlashSelenium/Pages/MeetingSurvey.java
+++ src/test/java/dhsFlashSelenium/Pages/MeetingSurvey.java
@@ -1,5 +1,7 @@
 package dhsFlashSelenium.Pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,20 +11,40 @@ import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 public class MeetingSurvey {
 	WebDriver driver;
 	
+	@FindBy(id="kudosForm")
+	public WebElement form;
+	
+	@FindBy(css="li[id^='kudos_question_']")
+	public List<WebElement> questions;
+	
+	@FindBy(css="input[id^='kudos_option_']")
+	List<WebElement> answers;
+	
 	@FindBy(css="button[title='Log In']")
-	public WebElement submitButton;
-
+	WebElement submitButton;
+	
 	public MeetingSurvey(WebDriver driver) {
 		this.driver = driver;
 		AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(driver, 15);
 		PageFactory.initElements(factory, this);
 	}
+	
+	public String getQuestion(int index) {
+		String question = questions.get(index).getText();
+		return question;
+	}
+	
+	public void selectTrue(int index) {
+		WebElement answer = answers.get(index);
+		answer.click();
+	}
+
+	public void selectFalse(int index) {
+		WebElement answer = answers.get(index + 1);
+		answer.click();
+	}
 
 	public void clickSubmit() {
 		submitButton.click();
 	} 
-	
-	public void submitMeetingSurvey() {
-		clickSubmit();
-	}
 }
